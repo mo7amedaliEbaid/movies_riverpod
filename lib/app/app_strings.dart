1bb1199c6d3facac5ff455956788f921089d5271
mo7amedaliEbaid <mo7amedaliebaid@gmail.com>
2023-09-10 02:34:04 +0300

    optimizing for apk

--- lib/app/app_strings.dart
+++ lib/app/app_strings.dart
@@ -1,5 +1,5 @@
 class AppStrings {
-  static const String appName = "FilmKu";
+  static const String appName = "CimaCima";
   static const String nowShowing = "Now Showing";
   static const String seeMore = "See More";
   static const String popular = "Popular";
